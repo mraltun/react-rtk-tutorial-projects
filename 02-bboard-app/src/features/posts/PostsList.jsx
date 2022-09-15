c2714ff43f700a2ded6464815082aa55f5b9a0a8
Murat Altun <mraltun@gmail.com>
2022-09-15 18:39:01 +0300

    Comments

--- 02-bboard-app/src/features/posts/PostsList.jsx
+++ 02-bboard-app/src/features/posts/PostsList.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
+  const orderedPosts = posts
+    // Slice to return shallow copy of the array
+    .slice()
+    // Compare date string and return negative or positive number then sort them.
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-  const renderedPosts = posts.map((post) => (
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
 
       <p className='postCredit'>
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
--- 02-bboard-app/src/features/posts/TimeAgo.jsx
+++ 02-bboard-app/src/features/posts/TimeAgo.jsx
@@ -3,7 +3,9 @@ import { parseISO, formatDistanceToNow } from "date-fns";
 const TimeAgo = ({ timestamp }) => {
   let timeAgo = "";
   if (timestamp) {
+    // Turn date to string
     const date = parseISO(timestamp);
+    // Return the distance between the given date and now in words.
     const timePeriod = formatDistanceToNow(date);
     timeAgo = `${timePeriod} ago`;
   }
