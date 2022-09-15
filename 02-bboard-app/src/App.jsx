d6510fa4299b4dc5072dee2ef68d58904954bd16
Murat Altun <mraltun@gmail.com>
2022-09-15 12:00:32 +0300

    Moved useSelector logic to slice

--- 02-bboard-app/src/App.jsx
+++ 02-bboard-app/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PostsList from "./features/posts/PostsList";
 
 const App = () => {
-  return <main className='App'>App</main>;
+  return (
+    <main className='App'>
+      <PostsList />
+    </main>
+  );
 };
 
 export default App;
