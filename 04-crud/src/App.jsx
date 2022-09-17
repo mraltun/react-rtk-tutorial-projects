6b4131c08129e6741bc5ee138acfd4d2fced8299
Murat Altun <mraltun@gmail.com>
2022-09-17 14:35:10 +0300

    Added routes to App

--- 04-crud/src/App.jsx
+++ 04-crud/src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <main className='App'>
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />} />
+      <Route index element={<PostsList />} />
+
+      <Route path='post'>
+        <Route index element={<AddPostForm />} />
+        <Route path=':postId' element={<SinglePostPage />} />
+      </Route>
+    </Routes>
   );
 };
 
