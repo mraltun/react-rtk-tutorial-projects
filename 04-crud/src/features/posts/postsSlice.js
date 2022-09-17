b60e0c8792d453db6205a0d53e199ea30323d5f4
Murat Altun <mraltun@gmail.com>
2022-09-17 14:19:53 +0300

    Added SinglePostPage

--- 04-crud/src/features/posts/postsSlice.js
+++ 04-crud/src/features/posts/postsSlice.js
@@ -120,6 +120,7 @@ const postsSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+export const selectPostById = (state, postId) => (post) => post.id === postId;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
