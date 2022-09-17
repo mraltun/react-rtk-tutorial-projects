15b389a782c95f56fc0b00093f752883185bb93d
Murat Altun <mraltun@gmail.com>
2022-09-17 14:31:13 +0300

    Added Layout component

--- 04-crud/src/index.js
+++ 04-crud/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 // Fetch the users right after the app loads
@@ -12,6 +13,10 @@ store.dispatch(fetchUsers());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
