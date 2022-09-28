af8442d53face6060d11e32f9f4955a7548291cd
Murat Altun <mraltun@gmail.com>
2022-09-28 19:19:39 +0300

    Added rendering the todo in TodoList

--- 06-rtkquery/src/features/todos/TodoList.jsx
+++ 06-rtkquery/src/features/todos/TodoList.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { useGetTodosQuery } from "../api/apiSlice";
+import {
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+} from "../api/apiSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 
@@ -13,9 +18,13 @@ const TodoList = () => {
     error,
   } = useGetTodosQuery();
 
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    //addTodo
+    addTodo({ userId: 1, title: newTodo, completed: false });
     setNewTodo("");
   };
 
@@ -42,7 +51,26 @@ const TodoList = () => {
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
-    content = JSON.stringify(todos);
+    content = todos.map((todo) => {
+      return (
+        <article key={todo.id}>
+          <div className='todo'>
+            <input
+              type='checkbox'
+              checked={todo.completed}
+              id={todo.id}
+              onChange={() =>
+                updateTodo({ ...todo, completed: !todo.completed })
+              }
+            />
+            <label htmlFor={todo.id}>{todo.id}</label>
+          </div>
+          <button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
   } else if (isError) {
     content = <p>{error}</p>;
   }
