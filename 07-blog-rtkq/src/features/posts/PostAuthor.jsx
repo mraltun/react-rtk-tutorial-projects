ae56e40b51ff7478e602f075da4d02fe6b4aeca5
Murat Altun <mraltun@gmail.com>
2022-09-29 16:21:35 +0300

    Added rest of the injectEndpoints

--- 07-blog-rtkq/src/features/posts/PostAuthor.jsx
+++ 07-blog-rtkq/src/features/posts/PostAuthor.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 
@@ -6,7 +7,16 @@ const PostAuthor = ({ userId }) => {
   const author = users.find((user) => user.id === userId);
 
   // The initial state objects doesn't have author field. So, they will shown as Unknown authors.
-  return <span>by {author ? author.name : "Unknown author"}</span>;
+  return (
+    <span>
+      by{" "}
+      {author ? (
+        <Link to={`/user/${userId}`}>{author.name}</Link>
+      ) : (
+        "Unknown author"
+      )}
+    </span>
+  );
 };
 
 export default PostAuthor;
--- 07-blog-rtkq/src/features/posts/postsSlice.js
+++ 07-blog-rtkq/src/features/posts/postsSlice.js
@@ -6,11 +6,11 @@ import {
 import { sub } from "date-fns";
 import { apiSlice } from "../api/apiSlice";
 
-const PostsAdapter = createEntityAdapter({
+const postsAdapter = createEntityAdapter({
   sortComparer: (a, b) => b.date.localeCompare(a.date),
 });
 
-const initialState = PostsAdapter.getInitialState({});
+const initialState = postsAdapter.getInitialState({});
 
 // Inject endpoints into the original API. Code-splitting friendly.
 export const extendedApiSlice = apiSlice.injectEndpoints({
@@ -34,17 +34,117 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
           return post;
         });
         // Normalize the data
-        return PostsAdapter.setAll(initialState, loadedPosts);
+        return postsAdapter.setAll(initialState, loadedPosts);
       },
       providesTags: (result, error, arg) => [
         { type: "Post", id: "LIST" },
         ...result.ids.map((id) => ({ type: "Post", id })),
       ],
     }),
+    getPostsByUserId: builder.query({
+      query: (id) => `/posts/?userId=${id}`,
+      transformResponse: (responseData) => {
+        let min = 1;
+        const loadedPosts = responseData.map((post) => {
+          if (!post?.date)
+            post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          if (!post?.reactions)
+            post.reactions = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+          return post;
+        });
+        return postsAdapter.setAll(initialState, loadedPosts);
+      },
+      providesTags: (result, error, arg) => [
+        ...result.ids.map((id) => ({ type: "Post", id })),
+      ],
+    }),
+    addNewPost: builder.mutation({
+      query: (initialPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: {
+          ...initialPost,
+          userId: Number(initialPost.userId),
+          date: new Date().toISOString(),
+          reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          },
+        },
+      }),
+      // Invalidate the Post & List cache
+      invalidatesTags: [{ type: "Post", id: "LIST" }],
+    }),
+    updatePost: builder.mutation({
+      query: (initialPost) => ({
+        url: `/posts/${initialPost.id}`,
+        method: "PUT",
+        body: {
+          ...initialPost,
+          date: new Date().toISOString(),
+        },
+      }),
+      // We get the id from argument
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
+    deletePost: builder.mutation({
+      query: ({ id }) => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
+    addReaction: builder.mutation({
+      query: ({ postId, reactions }) => ({
+        url: `posts/${postId}`,
+        method: "PATCH",
+        // In a real app, we'd probably need to base this on user ID somehow so that a user can't do the same reaction more than once
+        body: { reactions },
+      }),
+      async onQueryStarted(
+        { postId, reactions },
+        { dispatch, queryFulfilled }
+      ) {
+        // `updateQueryData` requires the endpoint name and cache key arguments, so it knows which piece of cache state to update
+        const patchResult = dispatch(
+          extendedApiSlice.util.updateQueryData(
+            "getPosts",
+            undefined,
+            (draft) => {
+              // The `draft` is Immer-wrapped and can be "mutated" like in createSlice
+              const post = draft.entities[postId];
+              if (post) post.reactions = reactions;
+            }
+          )
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
   }),
 });
 
-export const { useGetPostsQuery } = extendedApiSlice;
+export const {
+  useGetPostsQuery,
+  useGetPostsByUserIdQuery,
+  useAddNewPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+  useAddReactionMutation,
+} = extendedApiSlice;
 
 // Return the query result object
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select();
@@ -62,6 +162,6 @@ export const {
   selectById: selectPostById,
   selectIds: selectPostIds,
   // Pass in a selector that returns the posts slice of state
-} = PostsAdapter.getSelectors(
+} = postsAdapter.getSelectors(
   (state) => selectPostsData(state) ?? initialState
 );
